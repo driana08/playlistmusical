3e1643a47d6b2cd2165b8ccd721d8105ca74f747
driana08 <adrianavaldesprado08@gmail.com>
2024-05-30 17:17:21 -0400

    Lista criada

---
+++ index.js
@@ -0,0 +1,56 @@
+const generosMusicais = {
+    inicio: "Escolha o gênero musical que melhor se encaixa na música que quer adicionar",
+    generos: [
+        {
+            text: "Pop",
+            next: "pop"
+        },
+        {
+            text: "Rock",
+            next: "Rock"
+        },
+        {
+            text: "Jazz",
+            next: "Jazz"
+        },
+        {
+            text: "Funk",
+            next: "Funk"
+        },
+        {
+            text: "Sertanejo",
+            next: "Sertanejo"
+        },
+        {
+            text: "gospel",
+            next: "gospel"
+        },
+        {
+            text: "Musica classica",
+            next: "musicaClassica"
+        },
+        {
+            text: "Hip-Hop",
+            next: "HipHop"
+        },
+    ],
+    pop: [],
+    Rock: [],
+    Jazz: [],
+    Funk: [],
+    Sertanejo: [],
+    gospel: [],
+    musicaClassica: [],
+    HipHop: []
+
+}
+console.log (generosMusicais)
+
+let Escolha = generosMusicais
+
+ for (let contador = 0; contador < Escolha.generos.length; contador++) {
+    const escolhido = Escolha.generos[contador]
+ }
+
+ const opcoes = prompt ("Escolha um dos gêneros abaixo")
+
---
+++ readme.md
@@ -0,0 +1,2 @@
+Criar uma lista de gêneros musicais
+Pedir ao usuario que escolha um dos gêneros
