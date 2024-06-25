db12eaeef167e1780156ee61b39f833c001eefe0
driana08 <adrianavaldesprado08@gmail.com>
2024-06-25 19:02:46 -0400

    Finalmente deu certo

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const chooseGenres = {
+const history = {
+    event: "Escolha um gênero para adicionar uma música",
     generos: [
         {
             text: "Pop",
@@ -33,19 +34,76 @@ const chooseGenres = {
             next: "hipHop",
         },
     ],
-    pop: [],
-    rock: [],
-    jazz: [],
-    funk: [],
-    sertanejo: [],
-    gospel: [],
-    musicaClassica: [],
-    hipHop: [],
+    pop: {
+        event: "Escreva a música que deseja colocar",
+        musicas: [],
+    },
+    rock: {
+        event: "Escreva a música que deseja colocar",
+        musicas: [],
+    },
+    jazz: {
+        event: "Escreva a música que deseja colocar",
+        musicas: [],
+    },
+    funk: {
+        event: "Escreva a música que deseja colocar",
+        musicas: [],
+    },
+    sertanejo: {
+        event: "Escreva a música que deseja colocar",
+        musicas: [],
+    },
+    gospel: {
+        event: "Escreva a música que deseja colocar",
+        musicas: [],
+    },
+    musicaClassica: {
+        event: "Escreva a música que deseja colocar",
+        musicas: [],
+    },
 
 };
 
-let Escolhas = chooseGenres;
+let escolhas = history;
 
+const opcoesElement = document.getElementById("option")
+const eventElement = document.getElementById("event")
 
+function showMusic() {
+    if (escolhas.event) {
+        eventElement.innerHTML = escolhas.event;
+    }
+    else {
+        eventElement.innerHTML = escolhas.musicas;
+    }
+}
 
+function showOpcoes() {
+    opcoesElement.innerHTML = "";
+
+    if (!escolhas || !escolhas.generos) {
+        return;
+      }
+
+    for (let contador = 0; contador < escolhas.generos.length; contador++) {
+        const genres = escolhas.generos[contador];
+
+        const generoElement = document.createElement("button");
+        generoElement.classList.add("btn");
+        generoElement.innerHTML = genres.text;
+
+        generoElement.addEventListener("click", () => {
+            escolhas = history[genres.next];
+
+            showOpcoes();
+            showMusic();
+        });
+
+       opcoesElement.appendChild(generoElement);
+    }
+};
+
+showOpcoes();
+showMusic();
 
